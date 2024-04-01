7ff7f23b41ce5d7f3f1ca0c08b16af3cd790c5c1
jaybaradd <jaydharmishtha@gmail.com>
2024-04-01 15:08:33 +0530

    changed color scheme and added

--- KITapp/app/src/main/java/com/example/kitapp/loginActivity.java
+++ KITapp/app/src/main/java/com/example/kitapp/loginActivity.java
@@ -2,13 +2,28 @@ package com.example.kitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class loginActivity extends AppCompatActivity {
 
+public class loginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button nextBtn = findViewById(R.id.Nextbtn);
+    EditText email = findViewById(R.id.emailInput);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        nextBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getApplicationContext(), loginPasswordActivity.class);
+        startActivity(intent);
     }
 }
---
+++ KITapp/app/src/main/java/com/example/kitapp/username_activity.java
@@ -0,0 +1,37 @@
+package com.example.kitapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class username_activity extends AppCompatActivity implements View.OnClickListener {
+
+    Button nextBtn = findViewById(R.id.usernameNextbtn);
+    EditText username = findViewById(R.id.usernameInput);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_username);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    nextBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+    }
+}
